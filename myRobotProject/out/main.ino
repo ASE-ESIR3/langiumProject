f982e0379280a4dc60607f1b9c8ec7e90b9fc5cd
ronantremoureux <ronan.tremoureux@gmail.com>
2023-12-05 14:54:33 +0100

    add TEST RPJERCT

--- myRobotProject/out/main.ino
+++ myRobotProject/out/main.ino
@@ -4,7 +4,29 @@
 int square(int a){
 return a*a;
 }
+int test(int a,int b,int c,int d,int e,int f,int g,int h,int i,int j,int k,int l,int m,int n,int o,int p,int q,int r,int s,int t,int u,int v,int w,int x,int y,int z){
+return a+b+c+d+e+f+g+h+i+j+k+l+m+n+o+p+q+r+s+t+u+v+w+x+y+z;
+}
+int bruh(int a,int b){
+return a+b;
+}
+int recursive(int a){
+printf("%d",a<10);
+if (a<10)
+{
+printf("%d",a);
+return recursive(a+1);
+}
+
+
+else{
+return a;
+}
+;
+}
 void main(){
+printf("%d",recursive(0));
+printf("%d",bruh(5,4)*bruh(5,4));
 int a = 3;
 int stop = square(a)*square(a);
 int step = 10;
@@ -41,5 +63,15 @@ forward(10*0.1);
 
 }
 ;
+int b = 0;
+for (int val = 0; val<26; val = val+1){
+{
+b = b+val;
+printf("%d",b);
+}
+
+}
+;
+printf("%d",test(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26));
 return ;
 }
