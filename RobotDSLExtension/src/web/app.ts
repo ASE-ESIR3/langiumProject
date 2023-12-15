0cec4593c0866bc82a8bd7f6f72d219f61b33c76
ronan tremoureux <ronan.tremoureux@gmail.com>
2023-12-15 22:16:16 +0100

    add nocache

--- RobotDSLExtension/src/web/app.ts
+++ RobotDSLExtension/src/web/app.ts
@@ -1,6 +1,8 @@
 import express from 'express';
+import nocache from 'nocache';
 
 const app = express();
 const port = 3000;
 app.use(express.static('./public'));
+app.use(nocache());
 app.listen(port, () => { console.log(`Server for MyDsl assets listening on http://localhost:${port}`)});
